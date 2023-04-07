ab111cc3cd9b4fbd9ac41729ce2afc61d202c0a5
Khai-Git <kelvinnon28092001@gmail.com>
2023-04-07 12:30:20 +0700

    Fix code

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
-// const timeDisplay = document.querySelector('timeDisplay');
-const startBtn = document.querySelector('startTime');
-const stopBtn = document.querySelector('stopTime');
-const resetBtn = document.querySelector('resetTime');
+var startBtn = document.getElementById('startTime')
+var stopBtn = document.getElementById('stopTime')
+var resetBtn = document.getElementById('resetTime')
 
-startBtn.addEventListener("click", startTime);
-stopBtn.addEventListener("click", stopTime);
-resetBtn.addEventListener("click", resetTime);
+var msec = document.getElementById('msec')
+var sec = document.getElementById('seconds')
+var min = document.getElementById('minutes')
+var hrs = document.getElementById('hours')
 
-function startTimer() {
-    startBtn.classList.add("active")
+var msecCount = 0
+var secCount = 0
+var minCount = 0
+var hrsCount = 0
+
+var Interval
+
+startBtn.onclick = () => {
+    setInterval(() => {
+        // MiliSec to Sec
+        if (msecCount <= 9) {
+            msec.innerText = "0" + msecCount++
+        }
+        if (msecCount >= 10) {
+            msec.innerText = msecCount++
+        }
+        if (msecCount > 98) {
+            msecCount = 0
+
+            // Sec to Min
+            if (secCount <= 9) {
+                sec.innerText = "0" + secCount++
+            }
+            if (secCount >= 10) {
+                sec.innerText = secCount++
+            }
+            if (secCount > 59) {
+                secCount = 0
+                min.innerText = minCount++
+
+            // Min to Hrs
+                if (minCount < 10) {
+                    min.innerText = "0" + minCount++
+                }
+                if (minCount >= 10) {
+                    min.innerText = minCount++
+                }
+                if (minCount > 59) {
+                    minCount = 0
+                    hrs.innerText = hrsCount++
+                }
+            }
+        }
+    }, 1)
 }
 
-function stopTimer() {
-    stopBtn.classList.add("active")
-}
-
-function resetTimer() {
-    resetBtn.classList.add("active")
+stopBtn.onclick = () => {
+    setInterval(() => {
+        clearInterval(Interval)
+    })
 }
